9b04a69ebd15267eb4dcb6bb04f781903da0760e
Audrey <audreyb.edroso@gmail.com>
2025-07-01 14:29:00 +0200

    creation d'HTML en php

---
+++ Views/produit.html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main class="">
+        <div class="grid">
+            <img src="" alt="" class="">
+        </div>
+
+        <div class="">
+            <h3>NOM DU PRODUIT</h3>
+            <p class="">DESCRIPTION</p>
+            <h5>PRIX</h5>
+            <p>CATEGORIE</p>
+        </div>
+    </main>
+</body>
+</html>
