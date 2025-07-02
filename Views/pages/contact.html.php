bcc0e888982802d3dd536dd307253cfbceb591b6
Audrey <audreyb.edroso@gmail.com>
2025-07-02 12:52:50 +0200

    Ajout des pages 404, Accueil et Contact avec contenu HTML

---
+++ Views/pages/contact.html.php
@@ -0,0 +1,5 @@
+<h1>Contact</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit neque assumenda minus eius numquam natus nobis voluptatibus, aut reprehenderit at? Labore omnis in a, unde neque aliquam dolore perferendis dignissimos laudantium, eum nemo magnam pariatur exercitationem itaque minus adipisci quaerat tempora recusandae eius odio explicabo? Perferendis repudiandae, sit modi eos voluptatem id suscipit ratione unde expedita iste amet, eaque eum totam quasi nesciunt sed pariatur magni? Harum sapiente possimus recusandae perferendis cum repellendus architecto qui nulla quidem impedit culpa facilis aperiam, quibusdam molestias eaque odit consequatur illo rerum necessitatibus rem ipsam aliquid vero. Iste architecto rem fugiat natus, aperiam nisi?
+</p>
